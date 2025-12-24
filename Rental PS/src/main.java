376806aa62b3deb142ae2e7f240fbe7061d33291
rfniusmn4524144-sudo <rfniusmn4524144@univpancasila.ac.id>
2025-12-25 03:21:37 +0700

    menyelesaikan main

--- Rental PS/src/main.java
+++ Rental PS/src/main.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class main {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama penyewa: ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukkan lama sewa (jam): ");
+        int lama = input.nextInt();
+
+        System.out.println("=== DATA RENTAL PS ===");
+        RentalPS4 rental = new RentalPS4(nama, lama);
+        rental.tampilkanInfo();
+        rental.tampilkanBonus();
+
+        input.close();
+    }
 }
